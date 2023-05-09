7d9a709fb80627e4b8f9d407bed45111d0f225e1
Owner <vitycity79@gmail.com>
2023-05-09 09:52:15 +0300

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sizeOfArray = scanner.nextInt();
+        int[] array = new int[sizeOfArray];
 
-
+        for (int i = 0; i < sizeOfArray; i++){
+            array[i] = scanner.nextInt();
+        }
+        int counter = 99999999;
+        for (int i = 0; i < sizeOfArray; i++){
+            for (int ii = 1; ii < sizeOfArray; ii++){
+                if (array[i] < array[ii] && array[i] <= counter){
+                    counter = array[i];
+                    break;
+                }
+            }
+        }
+        System.out.println(counter);
     }
 }
